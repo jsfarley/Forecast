3d6fc5c521e3c31826afd9d76861095898670802
Sean Farley <james.s.farley@gmail.om>
2019-07-17 16:08:45 -0500

    data binding, CurrentWeather created to bind data from api to UI

--- app/src/main/java/com/jsfarley/forecast/CurrentWeather.java
+++ app/src/main/java/com/jsfarley/forecast/CurrentWeather.java
@@ -1,6 +1,59 @@
 package com.jsfarley.forecast;
 
 public class CurrentWeather {
+	private String locationValue;
+	private String icon;
+	private String summary;
+	private double temperature;
+	private double humidity;
+	private double precipChance;
+	private String timeZone;
+	private double lowTemp;
+	private double apparentTemp;
+
+	public CurrentWeather() {
+	}
+
+	public CurrentWeather(String locationValue, String icon, String summary, double temperature,
+	                      double humidity, double precipChance, String timeZone, double lowTemp,
+	                      double apparentTemp) {
+		this.locationValue = locationValue;
+		this.icon = icon;
+		this.summary = summary;
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.precipChance = precipChance;
+		this.timeZone = timeZone;
+		this.lowTemp = lowTemp;
+		this.apparentTemp = apparentTemp;
+	}
+
+	public double getLowTemp() {
+		return lowTemp;
+	}
+
+	public void setLowTemp(double lowTemp) {
+		this.lowTemp = lowTemp;
+	}
+
+	public double getApparentTemp() {
+		return apparentTemp;
+	}
+
+	public void setApparentTemp(double apparentTemp) {
+		this.apparentTemp = apparentTemp;
+	}
+
+
+	public String getTimeZone() {
+		return timeZone;
+	}
+
+	public void setTimeZone(String timeZone) {
+		this.timeZone = timeZone;
+	}
+
+
 	public String getLocationValue() {
 		return locationValue;
 	}
@@ -15,6 +68,43 @@ public class CurrentWeather {
 
 	public void setIcon(String icon) {
 		this.icon = icon;
+	}
+	public int iconId() {
+		int iconId = R.drawable.clear_day;
+		switch (icon) {
+			case "clear-day":
+				iconId = R.drawable.clear_day;
+				break;
+			case "clear-night":
+				iconId = R.drawable.clear_night;
+				break;
+			case "rain":
+				iconId = R.drawable.rain;
+				break;
+			case "snow":
+				iconId = R.drawable.snow;
+				break;
+			case "sleet":
+				iconId = R.drawable.sleet;
+				break;
+			case "wind":
+				iconId = R.drawable.wind;
+				break;
+			case "fog":
+				iconId = R.drawable.fog;
+				break;
+			case "cloudy":
+				iconId = R.drawable.cloudy;
+				break;
+			case "partly-cloudy-day":
+				iconId = R.drawable.partly_cloudy;
+				break;
+			case "partly-cloudy-night":
+				iconId = R.drawable.cloudy_night;
+				break;
+
+		}
+		return iconId;
 	}
 
 	public String getSummary() {
@@ -49,12 +139,6 @@ public class CurrentWeather {
 		this.precipChance = precipChance;
 	}
 
-	private String locationValue;
-	private String icon;
-	private String summary;
-	private double temperature;
-	private double humidity;
-	private double precipChance;
 
 
 }
--- app/src/main/java/com/jsfarley/forecast/MainActivity.java
+++ app/src/main/java/com/jsfarley/forecast/MainActivity.java
@@ -1,12 +1,18 @@
 package com.jsfarley.forecast;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+
+import com.jsfarley.forecast.databinding.ActivityMainBinding;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
@@ -25,16 +31,27 @@ public class MainActivity extends AppCompatActivity {
 	public static final String TAG = MainActivity.class.getSimpleName();
 
 	private CurrentWeather currentWeather;
+	private ImageView iconImageView;
+	final double latitude = 41.5236;
+	final double longitude = -90.5776;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		getForecast(latitude, longitude);
+	}
+
+	private void getForecast(double latitude, double longitude){
+		final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this,
+				R.layout.activity_main);
 
 		String apiKey = "bf0b68ba54650be9dd914dc48c709de2";
-		double latitude = 41.5236;
-		double longitude = -90.5776;
+		//double latitude = 41.5236;
+		//double longitude = -90.5776;
 		String forecastURL = "https://api.darksky.net/forecast/"+apiKey+ "/"+latitude+","+longitude;
+
+		iconImageView = findViewById(R.id.iconImageView);
 
 		if(isNetworkAvailable()) {
 			
@@ -56,6 +73,27 @@ public class MainActivity extends AppCompatActivity {
 						if (response.isSuccessful()) {
 							currentWeather = getCurrentDetails(jsonData);
 
+							final CurrentWeather displayWeather = new CurrentWeather(
+									currentWeather.getLocationValue(),
+									currentWeather.getSummary(),
+									currentWeather.getTimeZone(),
+									currentWeather.getTemperature(),
+									currentWeather.getHumidity(),
+									currentWeather.getPrecipChance(),
+									currentWeather.getIcon(),
+									currentWeather.getLowTemp(),
+									currentWeather.getApparentTemp()
+
+							);
+							binding.setWeather(displayWeather);
+							runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									Drawable drawable = (Drawable) getResources().getDrawable(displayWeather.iconId());
+
+									iconImageView.setImageDrawable(drawable);
+								}
+							});
 						} else {
 							alertUserAboutError();
 						}
@@ -71,9 +109,28 @@ public class MainActivity extends AppCompatActivity {
 
 	private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
 		JSONObject forecast = new JSONObject(jsonData);
+
 		String timezone = forecast.getString("timezone");
 		Log.i(TAG,"From JSON: " + timezone );
-		return null;
+
+		JSONObject currently = forecast.getJSONObject("currently");
+		JSONObject hourly = forecast.getJSONObject("hourly");
+
+		//Sets objects from CurrentWeather class and JSON data
+		CurrentWeather currentWeather = new CurrentWeather();
+		currentWeather.setHumidity(currently.getDouble("humidity"));
+		currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+		currentWeather.setIcon(currently.getString("icon"));
+		//This will be updated to be based upon gps location
+		currentWeather.setLocationValue("Rock Island, IL");
+		currentWeather.setSummary(hourly.getString("summary"));
+		currentWeather.setTemperature(currently.getDouble("temperature"));
+		currentWeather.setLowTemp(currently.getDouble("dewPoint"));
+		currentWeather.setApparentTemp(currently.getDouble("apparentTemperature"));
+		currentWeather.setTimeZone(timezone);
+
+
+		return currentWeather;
 	}
 
 	//checks for network and internet availability
